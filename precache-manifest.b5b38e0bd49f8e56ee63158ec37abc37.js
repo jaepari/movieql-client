43e5d4cf80cc0cb12ea39ca85e8df0ad62659f8b
김재훈 <jaehoonkim@gimjaehun-ui-MacBookPro.local>
2019-08-13 21:47:12 +0900

    Updates

---
+++ precache-manifest.b5b38e0bd49f8e56ee63158ec37abc37.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce324c8f6feb8449ecd692449b4f8ab1",
+    "url": "/movieql_client/index.html"
+  },
+  {
+    "revision": "279dd6a4ba3549d336e8",
+    "url": "/movieql_client/static/css/main.0e751715.chunk.css"
+  },
+  {
+    "revision": "66a6c2d9261b88cdc05e",
+    "url": "/movieql_client/static/js/2.8523f7e5.chunk.js"
+  },
+  {
+    "revision": "279dd6a4ba3549d336e8",
+    "url": "/movieql_client/static/js/main.f3f1ea94.chunk.js"
+  },
+  {
+    "revision": "85baf8054954e77623b9",
+    "url": "/movieql_client/static/js/runtime~main.0be87e8c.js"
+  }
+]);
